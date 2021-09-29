070d283835a8d8bb9381c95df93f74ff5118d081
Charnchanok Suwanmaneerattana <s62010102620058@email.kmutnb.ac.th>
2021-09-29 15:10:28 +0700

    Imrpove Css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_081958) do
+ActiveRecord::Schema.define(version: 2021_09_29_070234) do
+
+  create_table "moviegoers", force: :cascade do |t|
+    t.string "name"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
