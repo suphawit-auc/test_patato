2156c4196fc99736abbfcfe9b30e9d57683c4dfd
korrawee <korrawee.somyos@gmail.com>
2021-09-30 17:44:18 -0400

    Add expectation&DRY code(8.4 Finished)

--- spec/controller/movies_controller_spec.rb
+++ spec/controller/movies_controller_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 describe MoviesController, :type => :controller do
   describe 'searching TMDb' do
+	  before :each do
+		 @fake_results = [double('movie1'), double('movie2')]
+	  end
     it 'calls the model method that performs TMDb search' do
-      fake_results = [double('movie1'), double('movie2')]
       expect(Movie).to receive(:find_in_tmdb).with('hardware').
         and_return(fake_results)
       post :search_tmdb, params: {:search_terms => 'hardware'}
     end
-    it 'selects the Search Results template for rendering'
-    it 'makes the TMDb search results available to that template'
+
+    describe 'after valid search' do
+      before :each do
+      	allow(Movie).to receive(:find_in_tmdb).and_return(fake_results)
+      	post :search_tmdb, {:search_terms => 'hardware'}
+      end
+    end
+    it 'selects the Search Results template for rendering' do
+      expect(response).to render_template('search_tmdb')
+    end
+    it 'makes the TMDb search results available to that template' do
+      expect(assigns(:movies)).to eq(@fake_results)
+    end
   end
 end
