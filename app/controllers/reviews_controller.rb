3e2d5dfdd643017da36aebf788028d1d019ad7d7
Pakpoom-Phoonpunt <s6201012620210@kmutnb.ac.th>
2021-10-01 15:26:16 +0700

    add edit review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
-  before_action :has_moviegoer_and_movie, :only => [:new, :create]
+  before_action :has_moviegoer_and_movie, :only => [:new, :create , :edit]
     protected
     def has_moviegoer_and_movie
+      @movie = Movie.find_by_id(params[:movie_id])
       unless @current_user
-        flash[:warning] = 'You must be logged in to create a review.'
-        redirect_to '/'
+        flash[:warning] = 'You must be logged in to create or edit a review.'
+        redirect_to movie_path(@movie)
       end
       unless (@movie = Movie.where(:id => params[:movie_id]))
         flash[:warning] = 'Review must be for an existing movie.'
@@ -26,4 +27,33 @@ class ReviewsController < ApplicationController
       @current_user.reviews << @movie.reviews.build(params[:review].permit!)
       redirect_to movie_path(@movie)
     end
+
+    def edit
+      @movie = Movie.find_by_id(params[:movie_id])
+      @review = Review.find_by_id(params[:id])
+      if @current_user.uid != @review.moviegoer.uid
+        flash[:warning] = 'It not your review you can not edit'
+        redirect_to movie_path(@movie)
+      end
+    end
+
+    def update
+      @movie = Movie.find_by_id(params[:movie_id])
+      @review = Review.find_by_id(params[:id])
+      
+      if @review.update!(params[:review].permit(:potatoes , :comments))
+          flash[:notice] = "#{@movie.title} review was successfully updated."
+          redirect_to movie_path(@movie)
+      else 
+          render 'edit'
+      end
+    end
+    
+    def destroy
+      @movie = Movie.find_by_id(params[:movie_id])
+      @review = Review.find_by_id(params[:id])
+      @review.destroy()
+      flash[:notice] = "review id #{@review.id} was Deleted."
+      redirect_to movie_path(@movie)
+  end
   end
